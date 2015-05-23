c4c9e78cf3ca9e6bcb7bc0c8115339db0664b864
Kyle Galbraith <kyle.galbraith459@gmail.com>
2015-05-22 18:04:37 -0700

    Stubbed main program

--- src/QueryDemoApp/Program.cs
+++ src/QueryDemoApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cassandra;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,23 @@ namespace QueryDemoApp
 {
     class Program
     {
+        private static readonly string KEYSPACE_NAME = "demo";
         static void Main(string[] args)
         {
+            Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+            ISession session = cluster.Connect(KEYSPACE_NAME);
         }
+
+        static void QueryOne() { }
+
+        static void QueryTwo() { }
+
+        static void QueryThree() { }
+
+        static void QueryFour() { }
+
+        static void QueryFive() { }
+
+        static void QuerySix() { }
     }
 }
